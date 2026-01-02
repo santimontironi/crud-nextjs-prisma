15c525d5649c40ee81592bfe6fcc5e468ca748fa
santimontironi <santiimontironi@gmail.com>
2026-01-02 15:44:06 -0300

    api rest completada

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,39 +1,50 @@
 import { NextResponse } from "next/server";
-import {prisma} from "@/libs/prisma";
+import { prisma } from "@/libs/prisma";
 
 export async function GET(request, { params }) {
-    try{
-        const {id} = await params;
+    try {
+        const { id } = await params;
 
         const task = await prisma.task.findUnique({
             where: { id: Number(id) },
         });
 
-        return NextResponse.json({task}, {status:200});
+        return NextResponse.json({ task }, { status: 200 });
     }
-    catch(error){
-        return NextResponse.json({message:'Error al obtener la tarea', error: error.message}, {status:500});
+    catch (error) {
+        return NextResponse.json({ message: 'Error al obtener la tarea', error: error.message }, { status: 500 });
     }
 }
 
 export async function DELETE(request, { params }) {
-    try{
-        const {id} = await params;
+    try {
+        const { id } = await params;
 
-        return NextResponse.json({message: `Tarea con ID: ${id}`}, {status:200});
+        const task = await prisma.task.delete({
+            where: { id: Number(id) },
+        })
+
+        return NextResponse.json({ task }, { status: 200 });
     }
-    catch(error){
-        return NextResponse.json({message:'Error al eliminar la tarea', error: error.message}, {status:500});
+    catch (error) {
+        return NextResponse.json({ message: 'Error al eliminar la tarea', error: error.message }, { status: 500 });
     }
 }
 
 export async function PATCH(request, { params }) {
-    try{
-        const {id} = await params;
+    try {
+        const { id } = await params;
 
-        return NextResponse.json({message: `Tarea con ID: ${id}`}, {status:200});
+        const data = await request.json();
+
+        const task = await prisma.task.update({
+            where: { id: Number(id) },
+            data: data,
+        })
+
+        return NextResponse.json({ task }, { status: 200 })
     }
-    catch(error){
-        return NextResponse.json({message:'Error al editar la tarea', error: error.message}, {status:500});
+    catch (error) {
+        return NextResponse.json({ message: 'Error al editar la tarea', error: error.message }, { status: 500 });
     }
 }
